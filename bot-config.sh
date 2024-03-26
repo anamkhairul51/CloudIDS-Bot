cacb03f86e34e0c791f83ff155923999178c4d67
Khairul Anam <33059432+anamkhairul51@users.noreply.github.com>
2024-03-26 17:58:57 +0700

    Update bot-config.sh
    update api te;e

--- bot-config.sh
+++ bot-config.sh
@@ -9,8 +9,8 @@ logs=/home/c2007f0687/log-tele.txt
 msg_caption=/tmp/telegram_msg_caption.txt
 
 #Chat ID dan bot token Telegram
-chat_id="-683641881"
-token="5749168927:AAFjy55vGQRZoEcKSXcG-MlllFa40jd_Im4"
+chat_id="xxxxx"
+token="xxxxx"
 
 #kirim
 function sendAlert
